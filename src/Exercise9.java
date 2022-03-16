26d8407e04dd4bdb3cc13c4901f6e00f55de5107
maciejJachimowicz <99193047+MacJacOrg@users.noreply.github.com>
2022-03-16 09:15:17 +0100

    if

---
+++ src/Exercise9.java
@@ -0,0 +1,14 @@
+public class Exercise9 {
+    public static void main(String[] args) {
+        double i, j, d;
+
+        i = 5;
+        j = 10;
+
+        if (i != 0){
+            System.out.println("i jest różne 0");
+            d = j / i;
+            System.out.println("j / i = d " + d );
+        }
+    }
+}

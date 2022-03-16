1202a067258ef5cf91327aa4077494942d4c1259
maciejJachimowicz <99193047+MacJacOrg@users.noreply.github.com>
2022-03-16 09:05:45 +0100

    for

---
+++ src/Exercise8.java
@@ -0,0 +1,12 @@
+public class Exercise8 {
+    //pętla for
+
+    public static void main(String[] args) {
+        int count;
+
+        for (count = 0; count < 5;  count = count + 1) // pętla wykonuje 5 iteracji
+            System.out.println("Wartość zmiennej sterującej count: " + count);
+
+        System.out.println("Pętla wykonana");
+    }
+}
